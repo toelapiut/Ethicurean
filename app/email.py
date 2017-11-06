e49b32a4a030da75ab29b2a2432aab19f080e624
toelapiut <toelapiut7@gmail.com>
2017-11-06 09:32:04 +0300

    added email

--- app/email.py
+++ app/email.py
@@ -1,14 +1,20 @@
 from flask_mail import Message
 from flask import render_template
 from . import mail
+subject_pref = 'Ethicurean Fashion Blog'
+sender_email = 'toelapiut7@gmail.com'
 
 
 
 def mail_message(subject,template,to,**kwargs):
-    send_email="toelapiut@gmail.com"
 
     email=Message(subject,sender=sender_email,recipients=[to])
     email.body=render_template(template+'.txt',**kwargs)
-    email.html=render_template(template+"html",**kwargs)
+    email.html=render_template(template+".html",**kwargs)
     mail.send(email)
 
+def new_blog(subject,template,to,**kwargs):
+    email=Message(subject,sender=sender_email,recipients=[to])
+    email.body=render_template(template+'.txt',**kwargs)
+    email.html=render_template(template+".html",**kwargs)
+    mail.send(email)
