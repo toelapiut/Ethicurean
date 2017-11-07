f3b6725803bd04c20337be4f9c41dedf1ac15c1b
toelapiut <toelapiut7@gmail.com>
2017-11-07 10:50:24 +0300

    fx bugs

--- app/models.py
+++ app/models.py
@@ -1,33 +1,24 @@
+from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
 from flask_login import UserMixin
-from datetime import datetime
-from . import db
 from . import login_manager
-
-
+from datetime import datetime
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))
 class User(UserMixin,db.Model):
 	__tablename__='users'
 	id=db.Column(db.Integer,primary_key=True)
 	username=db.Column(db.String(255),unique=True,index=True)
 	email=db.Column(db.String(255),unique=True,index=True)
 	bio=db.Column(db.String(255))
-	profile_pic_path=db.Column(db.String(255))
 	password_hash = db.Column(db.String(255))
-	registered_on = db.Column(db.DateTime, nullable=False)
 	admin = db.Column(db.Boolean, nullable=False, default=False)
 	confirmed_on = db.Column(db.DateTime, nullable=True)
 	comments = db.relationship('Comment',backref = 'user',lazy="dynamic")
 	blogs=db.relationship('Blog',backref='user',lazy='dynamic')
 
 		
-	def __init__(self,username, email, password,
-				paid=False, admin=False, confirmed_on=None):
-		self.username=username
-		self.email = email
-		self.password = generate_password_hash(password)
-		self.registered_on = datetime.now()
-		self.admin = admin
-		self.confirmed_on = confirmed_on
 
 	@property
 	def password(self):
@@ -44,6 +35,7 @@ class User(UserMixin,db.Model):
 	def __repr__(self):
 		return f'User {self.username}'
 
+
 class Blog(db.Model):
 	__tablename__='blogs'
 	id=db.Column(db.Integer,primary_key=True)
@@ -51,6 +43,10 @@ class Blog(db.Model):
 	blog=db.Column(db.String(1000))
 	user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
 	comments=db.relationship('Comment',backref='blog',lazy='dynamic')
+
+	def save_blog(self):
+		db.session.add(self)
+		db.session.commit()
 
 class Comment(db.Model):
 	__tablename__='comments'
